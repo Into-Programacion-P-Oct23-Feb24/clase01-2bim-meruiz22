4d38b75b9704d847cb54bc4c8b0890608bd1e229
meruiz22 <meruiz22@utpl.edu.ec>
2023-12-20 08:14:12 -0500

    cambios

--- ManejoCicloFor/src/paquete03/Ejemplo03.java
+++ ManejoCicloFor/src/paquete03/Ejemplo03.java
@@ -13,6 +13,7 @@ import java.util.Scanner;
  * @author reroes
  */
 public class Ejemplo03 {
+
     /*Generar una solución que permita ingresar jugadores de fútbol; por cada 
     jugador se debe solicitar:
 - Nombre el jugador
@@ -30,22 +31,22 @@ Listado de Jugadores
 4. Ángel Mena -Delantero-, edad 32, estatura 1.75
 5. Michael Estrada -Delantero-, edad 27, estatura 1.93
     
-    */
-    
+     */
+
     public static void main(String[] args) {
         Scanner entrada = new Scanner(System.in);
         entrada.useLocale(Locale.US);
         String nombre;
         String posicion;
-        String listado= "Listadon de Jugadores";
+        String listado = "Listadon de Jugadores";
         int edad;
         double estatura;
         int limite;
-        int contador=0; 
+        int contador = 0;
         System.out.println("Ingrese el numero de jugadores que desea listar");
-        limite=entrada.nextInt();
+        limite = entrada.nextInt();
         entrada.nextLine();
-        for (int i = 0; i <limite; i++) {
+        for (int i = 0; i < limite; i++) {
             System.out.println("Ingrese el nombre del jugador: ");
             nombre = entrada.nextLine();
             System.out.println("Ingrese la posición en el campo: ");
@@ -55,16 +56,16 @@ Listado de Jugadores
             System.out.println("Ingrese la estatura del Jugador: ");
             estatura = entrada.nextDouble();
             entrada.nextLine();
-            contador = contador +1;
-            
-            listado= String.format("%s\n %d. %s -%s-, edad %d, estatura %.2f\n",
+            contador = contador + 1;
+
+            listado = String.format("%s\n %d. %s -%s-, edad %d, estatura %.2f",
                     listado,
                     contador,
                     nombre,
                     posicion,
                     edad,
                     estatura);
-            
+
         }
         System.out.printf("%s\n", listado);
     }
